2fdb4c63f9b7dc825827dbb3871f3f4f7d416f10
Macrohard-Onice <hyvhenthefox@gmail.com>
2024-07-09 11:21:19 +0200

    qzd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,7 @@ import { redirect } from "next/navigation";
 
 export default function Example() {
     //redirect to /caulnes
-
-
     redirect("/caulnes")
 
 
-    return (
-        <>
-        </>
-    )
 }

1ee7b0208b8542d36395620250b9424e620bea22
Jérémy Cochard <hougliloua@gmail.com>
2024-07-09 11:07:04 +0200

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,9 +4,9 @@ import { Header } from '@/components/Header'
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="fixed inset-0 flex bg-[#1c3430] justify-center sm:px-8">
+      <div className="fixed inset-0 flex bg-[#A0B0A5] justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
-          <div className="w-full bg-[#1c3430] " />
+          <div className="w-full bg-[#A0B0A5]" />
         </div>
       </div>
       <div className="relative flex w-full flex-col">

af667034fdbfd4c4a8912ce38c0daf52480fec4c
Macrohard-Onice <hyvhenthefox@gmail.com>
2024-07-10 12:43:04 +0200

    qz

--- src/app/[scope]/content.tsx
+++ src/app/[scope]/content.tsx
@@ -12,6 +12,7 @@ import avatarImage from '@/images/avatar.jpg'
 import Image from 'next/image'
 import { AppContext } from '../providers'
 import { Member } from '@/models/members'
+import { Scope } from '@/models/other'
 
 
 
@@ -128,7 +129,7 @@ Immobilier.notaires® : Evaluer, acheter & vendre avec les notaires partout en F
 
 export default function HomeContent({ members }: { members: Member[] }) {
 
-    const { etude, colors } = useContext(AppContext)
+    const { etude, colors, scope } = useContext(AppContext)
     console.log(members)
 
 
@@ -136,68 +137,68 @@ export default function HomeContent({ members }: { members: Member[] }) {
         <>
 
             <div className="relative isolate overflow-hidden pt-14">
-        <img
-          alt=""
-          src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
-          className="absolute inset-0 -z-10 h-full w-full object-cover"
-        />
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-              Accédez à l'étude de Caulnes - {' '}
-              <a href="#" className="font-semibold text-white">
-                <span aria-hidden="true" className="absolute inset-0" />
-                Visiter <span aria-hidden="true">&rarr;</span>
-              </a>
+                <img
+                    alt=""
+                    src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+                    className="absolute inset-0 -z-10 h-full w-full object-cover"
+                />
+                <div
+                    aria-hidden="true"
+                    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+                >
+                    <div
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                        className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                    />
+                </div>
+                <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
+                            Accédez à {scope == Scope.Cast ? "l'étude de Caulnes" : "l'étude de Cast"}  - {' '}
+                            <Link href={`/${scope == Scope.Cast ? Scope.Caulnes : Scope.Cast}`} className="font-semibold text-white">
+                                <span aria-hidden="true" className="absolute inset-0" />
+                                Visiter <span aria-hidden="true">&rarr;</span>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                            {etude.attributes.name}
+                        </h1>
+                        <p className="mt-6 text-lg leading-8 text-gray-300">
+                            {etude.attributes.description}
+                        </p>
+                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                            <a
+                                href="/rendezvous"
+                                style={{ background: colors.attributes.primary }}
+                                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                            >
+                                Prendre rendez-vous
+                            </a>
+                            <a href="#" className="text-sm font-semibold leading-6 text-white">
+                                Contact <span aria-hidden="true">→</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    aria-hidden="true"
+                    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+                >
+                    <div
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                    />
+                </div>
             </div>
-          </div>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                {etude.attributes.name}
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-                {etude.attributes.description}
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="/rendezvous"
-                style={{ background: colors.attributes.primary }}
-                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
-              >
-                Prendrez rendez-vous
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-white">
-                Contact <span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        >
-          <div
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          />
-        </div>
-      </div>
-      <Container>
+            <Container>
 
                 <div className="isolate w-full">
                     {/* Hero section */}
@@ -286,7 +287,7 @@ export default function HomeContent({ members }: { members: Member[] }) {
                                 <li key={member.attributes.name}>
                                     <img className="mx-auto h-24 w-24 rounded-full object-cover object-top" src={"https://adminpreview.hicards.fr" + (member.attributes.image.data ? member.attributes.image.data[0].attributes.url : "")} alt="" />
                                     <h3 className="mt-6  text-base font-semibold leading-7 tracking-tight  text-gray-900" style={{ color: colors.attributes.indicator }}>{member.attributes.name}</h3>
-                                    <p className=" text-sm leading-6  text-gray-600"  style={{ color: colors.attributes.hint }}>{member.attributes.role}</p>
+                                    <p className=" text-sm leading-6  text-gray-600" style={{ color: colors.attributes.hint }}>{member.attributes.role}</p>
                                 </li>
                             ))}
                         </ul>
--- src/models/etudes.ts
+++ src/models/etudes.ts
@@ -3,6 +3,26 @@
 import { Color } from "./colors";
 import { ApiRetrieveResponse, ObjectInterface } from "./other";
 
+export class EtudeOuvertures {
+    constructor(
+        public lundi: EtudeHoraires = { start: "", end: "" },
+        public mardi: EtudeHoraires = { start: "", end: "" },
+        public mercredi: EtudeHoraires = { start: "", end: "" },
+        public jeudi: EtudeHoraires = { start: "", end: "" },
+        public vendredi: EtudeHoraires = { start: "", end: "" }
+    ) {
+        this.lundi = lundi;
+        this.mardi = mardi;
+        this.mercredi = mercredi;
+        this.jeudi = jeudi;
+        this.vendredi = vendredi;
+    }
+}
+
+export interface EtudeHoraires {
+    start: string;
+    end: string;
+}
 export class Etude implements ObjectInterface {
     constructor(
         public id: string = "",
@@ -14,7 +34,8 @@ export class Etude implements ObjectInterface {
             schedules: "",
             phone: "",
             address: "",
-            slug: ""
+            slug: "",
+            ouvertures: new EtudeOuvertures(),
         },
     ) {
         this.id = id;

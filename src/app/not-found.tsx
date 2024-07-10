92e85dc05fc0c47e899d0accfdbc430a5918ad48
Macrohard-Onice <hyvhenthefox@gmail.com>
2024-07-10 11:05:56 +0200

    Delete things

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ import { useContext } from 'react'
 import { AppContext } from './providers'
 
 export default function NotFound() {
-  let { colors } = useContext(AppContext)
+  let { colors, etude } = useContext(AppContext)
   return (
     <Container className="flex h-full items-center pt-16 sm:pt-32">
       <div className="flex flex-col items-center">
@@ -13,13 +13,13 @@ export default function NotFound() {
           404
         </p>
         <h1 className="mt-4 dark:text-gray-200 text-4xl font-bold tracking-tight dark:text-gray-200 text-zinc-800 sm:dark:text-gray-200 text-5xl dark:dark:text-gray-200 text-zinc-100" style={{color: colors.attributes.accent}}>
-          Page not found
+          Page non trouvée
         </h1>
         <p className="mt-4 dark:text-gray-200 text-base dark:text-gray-200 text-zinc-600 dark:dark:text-gray-200 text-zinc-400" style={{color: colors.attributes.indicator}}>
-          Sorry, we couldn’t find the page you’re looking for.
+          Désolé, la page que vous cherchez n'existe pas.
         </p>
-        <Button href="/" variant="secondary" className="mt-4" style={{background: colors.attributes.primary}}>
-          Go back home
+        <Button href={`/${etude.attributes.slug}`} variant="secondary" className="mt-4" style={{background: colors.attributes.primary}}>
+          Retour à l'accueil
         </Button>
       </div>
     </Container>

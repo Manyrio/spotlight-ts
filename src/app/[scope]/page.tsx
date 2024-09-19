4752a45bb2e445c80b819192f91f695e30a771ea
Guillaume <84380559+Macrohard-Onice@users.noreply.github.com>
2024-09-19 14:19:29 +0200

    a

--- src/app/[scope]/page.tsx
+++ src/app/[scope]/page.tsx
@@ -1,23 +1,26 @@
 import { Method, call } from "@/scripts/api"
 import HomeContent from "./content"
 import { Etude } from "@/models/etudes"
-import { ApiListResponse } from "@/models/other"
+import { ApiListResponse, ApiRetrieveResponse } from "@/models/other"
 import { Member } from "@/models/members"
+import { CarouselComponent } from "@/models/carousel"
 
 
 
 export default async function Home() {
 
+  let carousel: ApiRetrieveResponse<CarouselComponent> = new ApiRetrieveResponse<CarouselComponent>
   let members: ApiListResponse<Member> = new ApiListResponse<Member>()
   try {
     members = await call("members?populate=*", Method.get)
+    carousel = await call("carousel?populate=*", Method.get)
   } catch (error) {
 
   }
 
 
   return (
-    <HomeContent members={members.data} />
+    <HomeContent members={members.data} carousel={carousel.data} />
   )
 
 }

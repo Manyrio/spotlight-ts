deef6314400cf663d1886a6662bee762800974bb
hyvhenthefox <hyvhenthefox@gmail.com>
2024-12-30 16:17:57 +0000

    Commit

--- src/app/api/annonces/[id]/route.tsx
+++ src/app/api/annonces/[id]/route.tsx
@@ -2,21 +2,8 @@ import { AnnonceObject } from "@/models/annonce";
 import { ApiListResponse } from "@/models/other";
 import { call, Method } from "@/scripts/api";
 import { NextRequest, NextResponse } from "next/server";
-import { syncAnnonces } from "@/scripts/syncAnnonces";
-
 export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
-
     let id = params.id
-
-    if (id == "sync") {
-        syncAnnonces()
-        return NextResponse.json({
-            message: "syncing"
-        }, {
-            status: 200,
-        })
-    }
-
     let response: ApiListResponse<AnnonceObject> = await call("annonces?populate=*&filters[uuid][$eq]=" + id, Method.get)
     console.log(response)
     let annonce = response.data.find(a => a.attributes.uuid == id) as AnnonceObject

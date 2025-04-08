722ffbcb37a5714d8d248635f106b842bdf7f643
contact.hicards <contact.hicards@gmail.com>
2025-04-08 08:48:05 +0000

    Commit

--- src/app/api/contact/route.tsx
+++ src/app/api/contact/route.tsx
@@ -1,17 +1,17 @@
 
+import { Etude } from '@/models/etudes'
 import { sendMail } from '@/scripts/sendMail'
 import { NextResponse } from 'next/server'
 export async function POST(req: any) {
 
-
-    // not needed in NextJS v12+
     const body = await req.json()
 
     let message = body["message"]
     let email = body["email"]
+    let etude: Etude = body["etude"]
 
     try {
-        await sendMail("Nouveau message de https://laube-lhomme-caulnes.notaires.fr/", "contact.hicards@gmail.com", message, email)
+        await sendMail("Nouveau message de https://laube-lhomme-caulnes.notaires.fr/", etude.attributes.email, message, email)
 
     } catch (error) {
 
